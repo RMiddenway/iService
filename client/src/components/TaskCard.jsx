b2d9f973f9cfd6816cfb825cc3f40aafde37b30c
Roger Middenway <rmiddenw@deakin.edu.au>
2021-10-17 12:10:36 +0200

    Add animation to task card

--- client/src/components/TaskCard.jsx
+++ client/src/components/TaskCard.jsx
@@ -1,4 +1,5 @@
-import { Card } from 'semantic-ui-react';
+import { useEffect, useState } from 'react';
+import { Card, Transition } from 'semantic-ui-react';
 
 const TaskCard = ({
   title,
@@ -9,32 +10,38 @@ const TaskCard = ({
   handleClick,
   isHighlighted,
 }) => {
+  const [visible, setVisible] = useState(false);
+  useEffect(() => {
+    setVisible(true);
+  }, []);
   return (
-    <Card
-      className={isHighlighted ? "bg-white" : "bg-light"}
-      raised
-      onClick={(e) => handleClick(taskId)}
-    >
-      <Card.Content>
-        <div>
-          <Card.Header
-            // as="h3"
-            className={isHighlighted ? "font-weight-bold color-teal" : ""}
-          >
-            {title}
-          </Card.Header>
-        </div>
-      </Card.Content>
-      <Card.Content>
-        <Card.Description>{description}</Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <div className="d-flex justify-content-between">
-          <div>{suburb || "Online"}</div>
-          <div>{date}</div>
-        </div>
-      </Card.Content>
-    </Card>
+    <Transition animation="pulse" duration={500} visible={visible}>
+      <Card
+        className={isHighlighted ? "bg-white" : "bg-light"}
+        raised
+        onClick={(e) => handleClick(taskId)}
+      >
+        <Card.Content>
+          <div>
+            <Card.Header
+              // as="h3"
+              className={isHighlighted ? "font-weight-bold color-teal" : ""}
+            >
+              {title}
+            </Card.Header>
+          </div>
+        </Card.Content>
+        <Card.Content>
+          <Card.Description>{description}</Card.Description>
+        </Card.Content>
+        <Card.Content extra>
+          <div className="d-flex justify-content-between">
+            <div>{suburb || "Online"}</div>
+            <div>{date}</div>
+          </div>
+        </Card.Content>
+      </Card>
+    </Transition>
   );
 };
 
