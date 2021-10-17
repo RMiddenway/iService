ad2d628622bdc7ec7417c12a308d62c3971ec05d
Roger Middenway <rmiddenw@deakin.edu.au>
2021-10-17 11:59:38 +0200

    Add transition to expert cards

--- client/src/components/ExpertCard.jsx
+++ client/src/components/ExpertCard.jsx
@@ -1,35 +1,33 @@
-import React from 'react';
-import { Card, Icon, Image, Rating, Reveal } from 'semantic-ui-react';
+import React, { useEffect, useState } from 'react';
+import { Card, Icon, Image, Rating, Reveal, Transition } from 'semantic-ui-react';
 
 const ExpertCard = ({ firstName, lastName, description, rating, imgURL }) => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    setVisible(true);
+  }, []);
+
   return (
     <>
-      <Reveal animated="fade">
-        <Reveal.Content visible></Reveal.Content>
-        <Reveal.Content hidden>
-          <Card className="bg-light">
-            <Image
-              src={imgURL ? imgURL : "https://i.pravatar.cc/300?sig=123"}
-              className="px-5"
-              ui={false}
-            />
-            <Card.Content>
-              <Card.Header>
-                {firstName} {lastName}
-              </Card.Header>
-              <Card.Description>{description}</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Rating
-                icon="star"
-                defaultRating={rating}
-                maxRating={5}
-                disabled
-              />
-            </Card.Content>
-          </Card>
-        </Reveal.Content>
-      </Reveal>
+      <Transition animation="pulse" duration={500} visible={visible}>
+        <Card className="bg-light">
+          <Image
+            src={imgURL ? imgURL : "https://i.pravatar.cc/300?sig=123"}
+            className="px-5"
+            ui={false}
+          />
+          <Card.Content>
+            <Card.Header>
+              {firstName} {lastName}
+            </Card.Header>
+            <Card.Description>{description}</Card.Description>
+          </Card.Content>
+          <Card.Content extra>
+            <Rating icon="star" defaultRating={rating} maxRating={5} disabled />
+          </Card.Content>
+        </Card>
+      </Transition>
     </>
   );
 };
