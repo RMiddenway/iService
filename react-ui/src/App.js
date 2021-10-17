15b5b6a2ec84f8fa624d5ae6545cf9dcba549638
Roger Middenway <rmiddenw@deakin.edu.au>
2021-10-17 03:19:07 +0200

    Troubleshooting

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,28 +1,29 @@
-import './App.css';
+import "./App.css";
 
-import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import { ToastProvider } from 'react-toast-notifications';
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom";
+import { ToastProvider } from "react-toast-notifications";
 
-import PrivateRoute from './auth/PrivateRoute';
-import NavBar from './components/NavBar';
-import TaskMap from './components/TaskMap';
-import BecomeExpert from './pages/BecomeExpert';
-import ExpertArea from './pages/ExpertArea';
-import FindTask from './pages/FindTask';
-import Home from './pages/Home';
-import MyTasks from './pages/MyTasks';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
-import TaskForm from './pages/TaskForm';
-import store from './store';
+import PrivateRoute from "./auth/PrivateRoute";
+import NavBar from "./components/NavBar";
+import TaskMap from "./components/TaskMap";
+import BecomeExpert from "./pages/BecomeExpert";
+import ExpertArea from "./pages/ExpertArea";
+import FindTask from "./pages/FindTask";
+import Home from "./pages/Home";
+import MyTasks from "./pages/MyTasks";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import TaskForm from "./pages/TaskForm";
+import store from "./store";
 
 function App() {
   // const isSignedIn = () => localStorage.getItem("IS_SIGNED_IN") === "true";
 
   return (
     <div className="App">
-      <Provider store={store}>
+      <div>Hello this is reaxt</div>
+      {/* <Provider store={store}>
         <ToastProvider>
           <Router>
             <NavBar />
@@ -39,7 +40,7 @@ function App() {
             </Switch>
           </Router>
         </ToastProvider>
-      </Provider>
+      </Provider> */}
     </div>
   );
 }
