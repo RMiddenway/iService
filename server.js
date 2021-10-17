bda0d602064c382b5c6e645857208337f641d2a7
Roger Middenway <rmiddenw@deakin.edu.au>
2021-10-17 04:07:43 +0200

    T

--- server.js
+++ server.js
@@ -539,7 +539,7 @@ app.get("/cancel", (req, res) => {
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "build", "index.html"));
   });
 }
 
